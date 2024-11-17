ab316aec17f4e17142dbe80ebea35ddf2d30ef27
machadooriginal <166729114+machadooriginal@users.noreply.github.com>
2024-11-17 09:49:41 -0300

    menu.js

--- JAVASCRIPT/menu.js
+++ JAVASCRIPT/menu.js
@@ -14,3 +14,45 @@ overlay.addEventListener('click', ()=>{
      menu.classList.remove('abrir-menu')
 })
 
+const links = document.querySelectorAll('a');
+
+links.forEach(link => {
+  link.addEventListener('click', (e) => {
+    // Remove a classe "active" de todos os links
+    links.forEach(link => link.classList.remove('active'));
+
+    // Adiciona a classe "active" ao link clicado
+    e.target.classList.add('active');
+  });
+});
+
+// menu mobile //
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+
+    const contactMessage = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    };
+
+    const messages = JSON.parse(localStorage.getItem('messages')) || [];
+
+
+    messages.push(contactMessage);
+
+    localStorage.setItem('messages', JSON.stringify(messages));
+
+
+    document.getElementById('contactForm').reset();
+    alert('Mensagem enviada com sucesso!');
+});
+
+// Slavar mensagens de contato //
+
+
+
+
+        
